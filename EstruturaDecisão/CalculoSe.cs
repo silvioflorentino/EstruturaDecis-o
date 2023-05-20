26ecadce2626c53287eba2e13f6f9d12f6786ab3
silvioflorentino <professorsilvioflorentino@yahoo.com.br>
2023-05-20 13:15:57 -0300

    Orientado ao objeto

---
+++ EstruturaDecisão/CalculoSe.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaDecisão
+{
+    public class CalculoSe
+    {
+       public string Calcular(int num01, int num02, int num03)
+        {
+
+           int maior;
+           string resul;
+            
+            if (num01 == num02 && num02 == num03)
+            {
+                resul = "Valores São Iguais";
+                return resul;
+            }
+
+            if (num01 == num03 && num01 > num02 && num03 > num02)
+            {
+                resul = "Valores " + num01 + " e  " + num03 + " São maiores.";
+                return resul;
+            }
+
+            if (num02 == num03 && num02 > num01 && num03 > num01)
+            {
+                resul = "Valores " + num02 + " e  " + num03 + " São maiores.";
+                return resul;
+            }
+
+            if (num01 == num02 && num01 > num03 && num02 > num03)
+            {
+                resul = "Valores " + num01 + " e  " + num02 + " São maiores.";
+                return resul;
+            }
+           
+            if (num01 > num02 && num01 > num03)
+            {
+                maior = num01;
+                return "O número " + maior + " é o maior.";
+            }
+            else if (num02 > num01 && num02 > num03)
+            {
+                maior = num02;
+                return "O número " + maior + " é o maior.";
+            }
+            else
+            {
+                maior = num03;
+                resul = "O número " + maior + " é o maior.";
+                return resul;
+            }
+            
+           // return maior;
+        }
+    }
+}
--- EstruturaDecisão/Estrutura.cs
+++ EstruturaDecisão/Estrutura.cs
@@ -17,49 +17,19 @@ namespace EstruturaDecisão
             InitializeComponent();
         }
 
+
+
         private void btnDescobrir_Click(object sender, EventArgs e)
         {
             int num01 = Convert.ToInt32(nNum01.Value);
             int num02 = Convert.ToInt32(nNum02.Value);
             int num03 = Convert.ToInt32(nNum03.Value);
-            int maior;
-            if (num01 == num02 && num02 == num03)
-            {
-                lblResultado.Text = "Valores São Iguais";
-                return;
-            }
 
-            if (num01 == num03 && num01 > num02 && num03 > num02)
-            {
-                lblResultado.Text = "Valores "+ num01 +" e  "+ num03 + " São maiores.";
-                return;
-            }
+            CalculoSe CalculoSe = new CalculoSe();
+            string maior=  CalculoSe.Calcular(num01, num02, num03);
 
-            if (num02 == num03 && num02 > num01 && num03 > num01)
-            {
-                lblResultado.Text = "Valores " + num02 + " e  " + num03 + " São maiores.";
-                return;
-            }
-
-            if (num01 == num02 && num01 > num03 && num02 > num03)
-            {
-                lblResultado.Text = "Valores " + num01 + " e  " + num02 + " São maiores.";
-                return;
-            }
-
-            if (num01 > num02 && num01 > num03)
-            {
-                maior = num01;
-            }else if(num02 > num01 && num02 > num03)
-            {
-                maior = num02;
-            }
-            else
-            {
-                maior = num03;
-            }
-
-            lblResultado.Text = "O número " + maior + " é o maior.";
+            lblResultado.Text = maior;
+           // lblResultado.Text = "O número " + maior + " é o maior.";
 
         }
     }
